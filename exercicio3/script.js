6773b20b4c92d6a5a787f13d83955b6486cd0e16
karoline254 <karolineexecut@gmail.com>
2022-09-12 14:00:20 -0300

    Exercicio 03

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+let posicao = 1 
+
+for(let i = 0; i < maioresPaises.length; i++){
+    console.log(`${posicao}: ${maioresPaises[i]}`)
+    posicao++
+}
